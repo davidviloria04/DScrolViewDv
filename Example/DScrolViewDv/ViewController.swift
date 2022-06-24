9954cd41ff45f837bcde6cafb2f66c1da7cfe3f2
David Viloria <david.viloria0424@gmail.com>
2022-06-24 18:34:03 -0500

    ScrollView agregada

--- Example/DScrolViewDv/ViewController.swift
+++ Example/DScrolViewDv/ViewController.swift
@@ -7,18 +7,33 @@
 //
 
 import UIKit
+import DScrolViewDv
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    let scrollView = DScrollView()
+        let scrollViewContainer = DScrollViewContainer(axis: .vertical, spacing: 10)
+        let scrollViewElement0 = DScrollViewElement(height: 1200, backgroundColor: .purple)
+        let scrollViewElement1 = DScrollViewElement(height: 500, backgroundColor: .purple)//.withBackground(image: #imageLiteral(resourceName: "icon.png"), contentMode: .scaleAspectFit)
+        
+        let label = UILabel()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+            view.backgroundColor = .white
+            label.text = "I am a label"
+            
+            view.addScrollView(scrollView,
+                               withSafeArea: .none,
+                               hasStatusBarCover: true,
+                               statusBarBackgroundColor: .white,
+                               container: scrollViewContainer,
+                               elements: [scrollViewElement0, scrollViewElement1])
+            
+            scrollViewElement1.addSubview(label)
+            label.edgeTo(scrollViewElement1)
+        }
 
 }
 
